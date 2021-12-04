bbc7aca74d962e6957ec844ed439eef0271625fd
Basically_Andrew <59465284+Basically-Andrew@users.noreply.github.com>
2021-12-03 23:52:00 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EarthMCTweaks
+ Enhance your gameplay with this Minecraft mod providing tweaks made for EarthMC.
---
+++ src/main/java/net/andre/emct/mixin/EarthMCTweaksMixin.java
@@ -0,0 +1,16 @@
+package net.andre.emct.mixin;
+
+import net.andre.emct.EarthMCTweaks;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class EarthMCTweaksMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		EarthMCTweaks.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
